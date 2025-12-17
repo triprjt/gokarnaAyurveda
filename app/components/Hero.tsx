50f6f0d10dfcac342b36cc203accce090e505b38
triprjt <triprjt@gmail.com>
2025-12-17 19:14:55 +0530

    changed color

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -102,9 +102,9 @@ export default function Hero() {
                         </div>
                     )}
                     <button className="md:hidden text-gray-700 hover:text-emerald-700" onClick={() => toggleMenu()}>
-                        {!isMenuOpen ? <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        {!isMenuOpen ? <svg className="w-6 h-6" fill="white" stroke="white" viewBox="0 0 24 24">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-                        </svg> : <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        </svg> : <svg className="w-6 h-6" fill="white" stroke="white" viewBox="0 0 24 24">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
                         </svg>}
                     </button>
